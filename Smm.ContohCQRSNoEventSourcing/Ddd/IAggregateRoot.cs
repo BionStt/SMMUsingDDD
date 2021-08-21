1afe15695618760edd0fd48a1b17878ecbe1f295
BionStt <43228630+BionStt@users.noreply.github.com>
2021-08-21 18:36:20 +0700

    ok

---
+++ Smm.ContohCQRSNoEventSourcing/Ddd/IAggregateRoot.cs
@@ -0,0 +1,21 @@
+﻿using Smm.ContohCQRSNoEventSourcing.Ddd.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smm.ContohCQRSNoEventSourcing.Ddd
+{
+    /// <summary>
+    ///  Aggregate root interface
+    /// </summary>
+    public interface IAggregateRoot
+    {
+        // IReadOnlyCollection<DomaintEvent> DomainEvents { get; }
+        //void ClearDomainEvents();
+
+        IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
+        void ClearDomainEvents();
+    }
+}
---
+++ Smm.ContohCQRSNoEventSourcing/Domain/EnumInEntity/JenisKelamin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smm.ContohCQRSNoEventSourcing.Domain.EnumInEntity
+{
+    public class JenisKelamin : AggregateRoot<int>
+    {
+        public String JenisKelaminKeterangan { get; set; }
+    }
+}
