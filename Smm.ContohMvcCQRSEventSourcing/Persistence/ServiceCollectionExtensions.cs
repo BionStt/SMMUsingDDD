8c4a9c62ec059091cbf5d5549e7df122e3b4d902
BionStt <43228630+BionStt@users.noreply.github.com>
2021-11-28 20:17:52 +0700

    ok

---
+++ Smm.ContohMvcCQRSEventSourcing/Persistence/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Smm.ContohMvcCQRSEventSourcing.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using MediatR;
+
+namespace Smm.ContohMvcCQRSEventSourcing.Persistence
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddContohCQRSESDbContext(this IServiceCollection services, string connectionString)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseSqlServer(connectionString));
+
+            //services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<LeasingRazorDbContext>().AddDefaultUI().AddDefaultTokenProviders();
+
+            //         services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+            //        .AddEntityFrameworkStores<AccountingDbContext>()
+            //        .AddDefaultUI()
+            //.AddDefaultTokenProviders();
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
--- Smm.ContohMvcCQRSEventSourcing/Program.cs
+++ Smm.ContohMvcCQRSEventSourcing/Program.cs
@@ -1,4 +1,29 @@
+using Smm.ContohMvcCQRSEventSourcing.BackgroundServices;
+using Smm.ContohMvcCQRSEventSourcing.DDD.EventDomain;
+using Smm.ContohMvcCQRSEventSourcing.DDD.Events;
+using Smm.ContohMvcCQRSEventSourcing.Persistence;
+using Smm.ContohMvcCQRSEventSourcing.Persistence.Event;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddContohCQRSESDbContext(builder.Configuration.GetConnectionString("LeasingRazorConnection"));
+
+
+
+// Infrastructure - Data EventSourcing
+builder.Services.AddScoped<IStoredEvents, StoredEvents>();
+builder.Services.AddSingleton<IEventSerializer, EventSerializer>();
+
+// Messaging
+builder.Services.AddScoped<IMessagePublisher, MessagePublisher>();
+builder.Services.AddScoped<IMessageProcessor, MessageProcessor>();
+
+// Message processing
+var section = builder.Configuration.GetSection(nameof(MessageProcessorServiceOptions));
+var messageProcessorTaskOptions = section.Get<MessageProcessorServiceOptions>();
+builder.Services.AddSingleton(messageProcessorTaskOptions);
+builder.Services.AddHostedService<MessagesProcessorService>();
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
